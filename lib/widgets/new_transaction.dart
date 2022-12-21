facbed2bed3027fcfab8e913a6efe9854d9b1030
Jason Ban Tze Tan <jtbt1999@go.byuh.edu>
2022-12-21 09:56:17 -1000

    Added Lists, Scrolling & Text Input

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final Function addTx;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: <
+            Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Title'),
+            controller: titleController,
+            // onChanged: (val) {
+            //   titleInput = val;
+            // },
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Amount'),
+            controller: amountController,
+            // onChanged: (value) => amountInput = value,
+          ),
+          TextButton(
+            child: Text('Add Transaction'),
+            style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.purple)),
+            onPressed: () {
+              addTx(
+                titleController.text, 
+                double.parse(amountController.text)
+                );
+            },
+          )
+        ]),
+      ),
+    );
+  }
+}
